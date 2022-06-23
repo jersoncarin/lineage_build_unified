94fd6ae4528c40f0db78eb7b34f46a7d48657cf7
Jerson Carin <63774442+jersoncarin@users.noreply.github.com>
2022-06-23 20:33:23 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 
+apply_patches() {
+    echo "Applying patch group ${1}"
+    bash ./lineage_build_unified/apply_patches.sh ./lineage_patches_unified/${1}
+}
+
 setupEnv() {
     echo "Setting up build environment"
     source build/envsetup.sh &> /dev/null
     echo "Setting up environment done"
     
-    echo "Repopicking changes"
-    repopick -Q "(status:open+AND+NOT+is:wip)+(label:Code-Review>=0+AND+label:Verified>=0)+project:LineageOS/android_packages_apps_Trebuchet+branch:lineage-19.1+NOT+332083"
-    repopick -t twelve-burnin
-    repopick -t qs-lightmode
-    repopick -t powermenu-lightmode
-    repopick 321337 # Deprioritize important developer notifications
-    repopick 321338 # Allow disabling important developer notifications
-    repopick 321339 # Allow disabling USB notifications
-    repopick 329229 -f # Alter model name to avoid SafetyNet HW attestation enforcement
-    repopick 329230 -f # keystore: Block key attestation for SafetyNet
-    repopick 329409 # SystemUI: screenshot: open the screenshot instead of edit
-    repopick 331534 -f # SystemUI: Add support to add/remove QS tiles with one tap
-    repopick 331791 # Skip checking SystemUI's permission for observing sensor privacy
-    echo "Repopicking done"
+#     echo "Repopicking changes"
+#     repopick -Q "(status:open+AND+NOT+is:wip)+(label:Code-Review>=0+AND+label:Verified>=0)+project:LineageOS/android_packages_apps_Trebuchet+branch:lineage-19.1+NOT+332083"
+#     repopick -t twelve-burnin
+#     repopick -t qs-lightmode
+#     repopick -t powermenu-lightmode
+#     repopick 321337 # Deprioritize important developer notifications
+#     repopick 321338 # Allow disabling important developer notifications
+#     repopick 321339 # Allow disabling USB notifications
+#     repopick 329229 -f # Alter model name to avoid SafetyNet HW attestation enforcement
+#     repopick 329230 -f # keystore: Block key attestation for SafetyNet
+#     repopick 329409 # SystemUI: screenshot: open the screenshot instead of edit
+#     repopick 331534 -f # SystemUI: Add support to add/remove QS tiles with one tap
+#     repopick 331791 # Skip checking SystemUI's permission for observing sensor privacy
+#     echo "Repopicking done"
+    
+    echo "Applying patches"
+    apply_patches patches_treble_prerequisite
+    apply_patches patches_treble_phh
+    apply_patches patches_platform
+    apply_patches patches_treble
+    echo "Done applying patches"
 }
 
 setupEnv
