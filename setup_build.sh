cf309f938290e56a7d7ccf2cfe10560cb35fb9a6
Jerson Carin <63774442+jersoncarin@users.noreply.github.com>
2022-06-23 20:55:27 +0800

    Update setup_build.sh

--- setup_build.sh
+++ setup_build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BUILD_DATE="$(date +%Y%m%d)"
+
 apply_patches() {
     echo "Applying patch group ${1}"
     bash ./lineage_build_unified/apply_patches.sh ./lineage_patches_unified/${1}
@@ -33,4 +35,45 @@ setupBuilds() {
     echo "Done applying patches"
 }
 
+clean_build() {
+    echo "Cleaning the build treble"
+    rm -f device/*/sepolicy/common/private/genfs_contexts
+    cd device/phh/treble
+    git clean -fdx
+    bash generate.sh lineage
+    cd ../../..
+    echo "Done cleaning"
+}
+
+buildVanilla() {
+    echo 'Building Lineage 19.1 Vanilla...'
+    lunch lineage_arm64_bvS-userdebug
+    make installclean
+    make -j8 systemimage
+    mv $OUT/system.img ~/build-output/lineage-19.1-$BUILD_DATE-UNOFFICIAL-arm64_bvS.img
+    make vndk-test-sepolicy
+    echo 'Done building...'
+}
+
+buildGapps() {
+    echo 'Building Lineage 19.1 with Gapps...'
+    lunch lineage_arm64_bgS-userdebug
+    make installclean
+    make -j8 systemimage
+    mv $OUT/system.img ~/build-output/lineage-19.1-$BUILD_DATE-UNOFFICIAL-arm64_bgS.img
+    make vndk-test-sepolicy
+    echo 'Done building...'
+}
+
 setupBuilds
+
+# Clean the build
+clean_build
+
+# Create a output folder 
+# in the home directory
+mkdir -p ~/build-output
+
+# Build it
+buildVanilla
+buildGapps
